9c85de422f74c38d36e5d68117ed707f1b28c3c9
amandaleee <amandalee.anderson@gmail.com>
2016-01-17 02:40:37 -0600

    trying to defuck this form

--- index.php
+++ index.php
@@ -67,8 +67,8 @@ $zip=$_POST['zip'];
 //form variables CHECKBOXES - coming back to this
 
 //email message
-$message="Please add $name to the postcarding list for this month.
-$email
+$message= "Please add $name to the postcarding list for this month.<br />
+$email 
 $name
 $address
 $city
