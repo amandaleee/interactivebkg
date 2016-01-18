7bf1aaf61505bf345fa600b0dab1d66eadccc184
amandaleee <amandalee.anderson@gmail.com>
2016-01-18 01:00:44 -0600

    jpg,maybe still huge

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<script src="../file/js/modernizr.js"></script>
 	</head>
 	<body>
-		<div class="container bg" data-ibg-bg="background-january.png">
+		<div class="container bg" data-ibg-bg="background-january.jpg">
 			<div class="addressform">
 				<h1>Want a postcard? <br />Enter your info here.</h1>
 				<p class="smaller">All fields are required.</p>
