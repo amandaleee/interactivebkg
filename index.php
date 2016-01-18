2255489e9b14742dce8c57ff74359e1c98c09a81
amandaleee <amandalee.anderson@gmail.com>
2016-01-17 23:34:59 -0600

    updates

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 		<meta name="author" content="" /><!-- 
 		<link rel="shortcut icon" href="../file/favicon.gif">  -->
     	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/default.css" />
    		<script type="text/javascript" src="js/jquery.interactive_bg.js"></script>
 		<!-- Edit Below -->
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
@@ -20,21 +21,21 @@
 			<div class="addressform">
 				<h1>Want a postcard? Enter your info here.</h1>
 				<form class="sendform" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-					<input type="text" placeholder="name" name="name" required />
-					<input type="text" placeholder="address" name="address" required />
-					<input type="text" placeholder="city" name="city" required />
-					<input type="text" placeholder="state" name="state" required />
-					<input type="text" placeholder="zip" name="zip" required />
-					<input type="text" placeholder="country" name="country" required />
-					<input type="text" placeholder="email" name="email" required />
+					<input type="text" placeholder="name (required)" name="name" />
+					<input type="text" placeholder="address" name="address" />
+					<input type="text" placeholder="city" name="city" />
+					<input type="text" placeholder="state" name="state" />
+					<input type="text" placeholder="zip" name="zip" />
+					<input type="text" placeholder="country (required)" name="country" />
+					<input type="text" placeholder="email (required)" name="email" />
 					<label for="notabot" class="notabot" name="notabot">I'm not a bot
 						<input type="checkbox" value="i'm not a bot" name="notabot" />
 					</label>
-					<input type="submit" value="send me a postcard" />
+					
+					<input type="submit" value="sendmeapostcard" />
 				</form>
 				<?php if (isset($_POST['submit'])) : ?>
 				   <p>Thank you for subscribing!</p>
-
 				<?php endif; ?>
 			</div>
 		
@@ -42,14 +43,17 @@
         $(".bg").interactive_bg();
       </script>
       <footer>
-      	<a href="mailto:amandalee.anderson@gmail.com">Contact me</a>
+      	<a href="#">What's this?</a>
+      	<a href="#">FAQ</a>
+      	<!-- <a href="#">Tip me [for the postage]</a> -->
+      	<a href="mailto:#">Contact me</a>
       </footer>
 
 		</div><!-- Container -->
 		<?php 
-			$to='amandalee.anderson@gmail.com';
+			// vars from form
 
-			//form variables TEXTFIELDS
+			$to='amandalee.anderson@gmail.com	';
 			$email=$_POST['email'];
 			$name=$_POST['name'];
 			$address=$_POST['address'];
@@ -57,31 +61,16 @@
 			$state=$_POST['state'];
 			$country=$_POST['country'];
 			$zip=$_POST['zip'];
-			
-			//form variables CHECKBOXES - coming back to this
 
-			//subjects
-			$ala_subject='$name wants a postcard! ';//update this
-			$recipient_subject= 'You\'ve signed up to receive a postcard!';
+			//subject
+			$subject='$name wants a postcard!';
 
-			//messages
-			$ala_message= 'Please add $name to the postcarding list for this month. $email $name $address $city $state $country $zip THANKSSSS';
-			$recipient_message = 'Hi! Thx for your submission. Here\'s the info we got from you. Email us if there\'s anything amiss. ALA will email you when these are being mailed.';
-
-			//actually sending
-			mail($to,$ala_subject,$ala_message); 
-			// mail($email,$recipient_subject,$recipient_message);
-		?>
+			//email message
+			$message= "Please add $name to the postcarding list for this month. $email $name $address $city $state $country $zip THANKSSSS";
+			mail($to,$subject,$message); ?>
 		
 	</body>
-	
 
-	<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		  ga('create', 'UA-2700135-20', 'auto');
-		  ga('send', 'pageview');
-	</script>
+
+	
 </html>
