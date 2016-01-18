65f5b8bf326960f882a6d9be90a2a5760fc6522a
amandaleee <amandalee.anderson@gmail.com>
2016-01-17 23:38:39 -0600

    updating analytics again

--- index.php
+++ index.php
@@ -63,14 +63,20 @@
 			$zip=$_POST['zip'];
 
 			//subject
-			$subject='Someone wants a postcard! $name';
+			$subject='Someone wants a postcard!';
 
 			//email message
 			$message= "Please add $name to the postcarding list for this month. $email $name $address $city $state $country $zip THANKSSSS";
 			mail($to,$subject,$message); ?>
 		
 	</body>
-
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-2700135-20', 'auto');
+		  ga('send', 'pageview');
+</script>
 
 	
 </html>
